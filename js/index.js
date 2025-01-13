bee8710134a1a17a14c9a48e4bc4263999c9a1f5
Diego Barrero <dbarrero.789@gmail.com>
2025-01-13 10:09:57 -0500

    feat: :tada: start picas y fijas javascript review

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+let numeroEncontrado = false
+const NUMERO = 9410
+let numeroEvaluar = ""
+
+do {
+    let numContrincante = prompt("Ingrese el numero a evaluar: ")
+    numerosRepetidos = false
+
+    
+
+
+} while (!numeroEncontrado)
+
+
+
+
+// print(""""
+//       ==============================================
+//       ======= Welcome to picas and fijas game ======
+//       ==============================================
+//       """)
+
+// NUMERO = "9410"
+// numeroEncontrado = False
+// numeroEvaluar = ""
+
+// while not numeroEncontrado:
+//     numContrincante = input("Ingrese el numero a evaluar: ")
+//     numeros_repetidos = False
+//     for num in numContrincante:
+//         if numContrincante.count(num) > 1:
+//             numeros_repetidos = True
+//     if numeros_repetidos:
+//         print("No se pueden ingresar numeros repetidos")
+//     else:
+//         picas = 0
+//         fijas = 0
+
+//         if NUMERO == numContrincante:
+//             numeroEncontrado = True
+//         else:
+//             #Buscar las fijas
+//             for i in range(0, 4):
+//                 x = NUMERO.find(numContrincante[i])
+//                 if x == i:
+//                     fijas += 1
+//                 elif x != -1:
+//                     picas += 1
+//             print(f"Tiene {picas} picas y {fijas} fijas.")
+
+//         #para arreglar
+//         #numeroEncontrado = True
+
+//     if numeroEncontrado:
+//         print( "Has encontrado el numero!")
